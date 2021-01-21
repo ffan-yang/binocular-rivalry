20f60bcc73ac52a12c153c554e0485e078c748f1
ffan-yang <fan.yang@u-psud.fr>
2021-01-21 11:19:00 +0100

    question 5 solved, need to make better pictures and to find transient state duration

--- main.m
+++ main.m
@@ -15,14 +15,33 @@ R = 0.95;
 
 Ts = 0.001;
 stop_time = 20;
+N = stop_time/Ts;
 
-out = sim('model');
+end_graph = 3.5;
+step = 0.1;
 
+N_graph = end_graph/step;
+max_values_El = zeros(N_graph, 1);
+min_values_El = zeros(N_graph, 1);
+max_values_Er = zeros(N_graph, 1);
+min_values_Er = zeros(N_graph, 1);
 
-plot(out.El);
+i = 1;
+
+for a = 0.0:step:end_graph
+    out = sim('model');
+    % TODO: Check that transient state lasts until N/2!
+    
+    max_values_El(i) = max(out.El.Data(N/2:end));
+    min_values_El(i) = min(out.El.Data(N/2:end));
+    max_values_Er(i) = max(out.Er.Data(N/2:end));
+    min_values_Er(i) = min(out.Er.Data(N/2:end));
+    i = i + 1;
+    
+
+end
+
+plot(max_values_El);
 hold on
-plot(out.Hl);
-hold on
-plot(out.Er);
-hold on
-plot(out.Hr);
+plot(min_values_El);
+
